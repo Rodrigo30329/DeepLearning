64e764a6a98b80ac09dd3228ebefd60f92a1588d
Rodrigo30329 <joserodrigoavilacastro@gmail.com>
2022-08-15 18:24:59 -0500

    git committ

---
+++ UsoBasicoMatlplotlib.py
@@ -0,0 +1,28 @@
+import cv2
+import matplotlib.pyplot as plt
+
+def main():
+    imgpath = "C:\\Users\\joser\\Pictures\\Saved Pictures\\DeepL\\Originales\\Imagen1.jfif"
+    img = cv2.imread(imgpath, 0)
+
+    plt.imshow(img)
+    plt.title('Imagen por Defecto')
+    plt.show()
+
+    plt.imshow(img, cmap='Blues')
+    plt.title('Imagen en Azules')
+    plt.show()
+
+    plt.imshow(img, cmap='Spectral')
+    plt.title('Imagen en Espectral')
+    plt.show()
+
+    plt.imshow(img, cmap='gray')
+    plt.title('Imagen en Grises')
+    # Ocultar Ejes
+    # plt.yticks([])
+    # plt.xticks([])
+    plt.show()
+    
+if __name__=="__main__":
+    main()
