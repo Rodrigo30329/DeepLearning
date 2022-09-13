0f0d796fcccd5fd32c6a9033ad42b0528e3d8250
Rodrigo30329 <joserodrigoavilacastro@gmail.com>
2022-09-13 16:39:30 -0500

    commit

---
+++ MLP/MLP.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.models import Dense
+
